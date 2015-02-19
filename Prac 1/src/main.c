580059e66c7bf95e50383407ee60941585f7be44
Sean (WDXHP8) <sean@anotherway.co.za>
2015-02-19 22:43:23 +0200

    Initial commit

---
+++ Prac 1/src/main.c
@@ -0,0 +1,32 @@
+/**
+ *
+ * EEE3061W Practical 1
+ * Author: Sean Wood | WDXSEA003
+ *
+ */
+
+#include <stdio.h>
+#include "diag/Trace.h"
+
+// ----- main() ---------------------------------------------------------------
+
+// Sample pragmas to cope with warnings. Please note the related line at
+// the end of this function, used to pop the compiler diagnostics status.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+
+int main(int argc, char* argv[]) {
+	// At this stage the system clock should have already been configured
+	// at high speed.
+
+	// Infinite loop
+	while (1) {
+		// Add your code here.
+	}
+}
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------------
